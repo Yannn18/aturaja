447a9826e781f1341f923ce666ad805aecc4a864
Yannn18 <azaryayanuar18@gmail.com>
2026-04-10 17:09:17 +0700

    navigation bar widget as main wrapper

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:aturaja/ui/screens/app.dart';
 import 'package:flutter/material.dart';
 import 'core/theme/app_theme.dart';
 import 'ui/screens/home/home_screen.dart';
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
       initialRoute: '/splash',
       routes: {
         '/splash': (context) => const SplashScreen(),
-        '/home': (context) => const HomeScreen(),
+        '/home': (context) => const App(),
       },
     );
   }
